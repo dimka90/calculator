d2de9d68e606bc3a56043da5d3df9b7becb1d219
dimka90 <dimkayilrit@gmail.com>
2022-12-15 11:55:19 +0100

    addded the delete functionality and class

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 A simple calculator that performs basic arithmetic:addition(+), Subtraction(-),division and multiplication
 The calculator contains 
 *Clear  button
+Solving simple quadratic calculations at the same time
--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ function getUserClick(){
     let equalBtn=document.querySelector(".button-two");
     let display=document.querySelector(".screen");
     let clearBtn=document.querySelector(".button-clr");
+    let deleteBtn=document.querySelector(".button-del");
     // let operator=document.querySelectorAll(".operand");
    operand.forEach((button)=>{
     button.addEventListener("click",(e)=>{
@@ -23,6 +24,10 @@ display.textContent=result;
 clearBtn.addEventListener("click",(e)=>{
     display.textContent=""
 
+});
+deleteBtn.addEventListener("click",(e)=>{
+let delNum=display.textContent.slice(0,-1);
+display.textContent=delNum;
 })
 }
 
